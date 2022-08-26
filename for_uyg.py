0a8c1f19ade0dd00cf1a07eca3d731bacc3dec24
Goksel Ozkaya <goksel.ozkaya@hotmail.com>
2022-08-26 15:37:25 +0300

    for uygulaması

---
+++ for_uyg.py
@@ -0,0 +1,13 @@
+sayilar = [1, 3, 5, 7, 9, 12, 19, 21]
+#sayılar listesindeki hangi sayılar üçün katı?
+
+
+#toplamları ne?
+
+
+top = 0
+for _ in sayilar:
+    top += _
+print("top") 
+
+#tek sayıların karesi
