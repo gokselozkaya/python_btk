e1ac2006efc1e7505043e8eb6f71899d1dc3c3db
Goksel Ozkaya <goksel.ozkaya@hotmail.com>
2022-08-31 18:52:49 +0300

    lambda konusu

--- tahm_say.py
+++ tahm_say.py
@@ -1,1 +1,46 @@
+'''
 #kelimeyi belirtilen kez göster
+def shw(x,y):
+    return x*y
+shw("selam\n", 10)
+#gönderilen parametreleri listeye çevir
+def mklist(*params):
+    list1= []
+    for p in params:
+        list1.append(p)
+    return list1
+res = mklist(5,9,"hello",96,[5,8])
+print(res)
+
+# sayı asal mıdır?
+sayi = int(input("sayı giriniz: "))
+asaldır = True
+
+if sayi == 1:
+    asaldır = False
+
+for num in range(2, sayi):
+    if (sayi % num == 0):
+        asaldır= False
+        break
+
+if asaldır:
+    print(f"{sayi} asal sayıdır")        
+else: 
+    print(f"{sayi} asal değil")
+'''
+
+def asalbul(x,y):
+        for num in range(x,y):
+            if num>1:
+                for i in range (2,num):
+                 if num%i ==0:
+                    break
+                else:
+                    print(num)
+
+
+res = asalbul(2,9)
+print(res)  
+
+        
